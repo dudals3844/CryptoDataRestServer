5681e08f6aa0531c9f5f12fc9f7370e94ec098f5
YoungMinChoi <dudals3844@gmail.com>
2022-03-18 16:38:11 +0900

    Bulk insert 형식으로 변경

--- upbit/views.py
+++ upbit/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import viewsets, status
 from .models import (
     CandleChartModel,
     MarketTickerModel,
@@ -20,6 +22,17 @@ class CandleChartView(viewsets.ModelViewSet):
     queryset = CandleChartModel.objects.all()
     serializer_class = CandleChartSerializer
     pagination_class = PostPageNumberPagination
+
+    def create(self, request, *args, **kwargs):
+        candle_data = request.data
+        serializer = self.get_serializer(data=candle_data, many=True)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def perform_create(self, serializer):
+        serializer.save()
 
 
 class MarketTickerView(viewsets.ModelViewSet):
